3dff1e3fff6003e0901d98c23b24b29429a7be23
Andru6a <andrew.pmnv@gmail.com>
2023-10-05 13:23:12 +0300

    second app lesson 24 v1

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 'use strict';
-// lesson 18
+/* Задание на урок:
+
+// 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+// 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+// "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+// "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+
+// lesson 18, 23
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 let personalMovieDB = {
@@ -10,14 +26,59 @@ let personalMovieDB = {
 	privat: false
 };
 
-const questionLastFilm = prompt('Один из последних просмотренных фильмов', ''),
-    questionGoal = +prompt('На сколько оцените его', ''),
-    questionLastFilm2 = prompt('Один из последних просмотренных фильмов', ''),
-    questionGoal2 = +prompt('На сколько оцените его', '');
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов', '');
+    const b = prompt('На сколько оцените его', '');
+    if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done')
+    } else {
+        alert('Ошибка, давайте по новой!')
+        i--;
+    }
+}
 
-personalMovieDB.movies[questionLastFilm] = questionGoal;
-personalMovieDB.movies[questionLastFilm2] = questionGoal2;
 
+// 2 вариант
+// let i = 0;
+// do{ 
+//     const a = prompt('Один из последних просмотренных фильмов', '');
+//     const b = prompt('На сколько оцените его', '');
+//     i++;
+//     if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done')
+//     } else {
+//         alert('Ошибка, давайте по новой!')
+//         i--;
+//     }
+//     console.log('done1');
+// } while (i < 2 );
+
+// 3 вариант
+// let i = 0;
+// while (i < 2 ){
+//     const a = prompt('Один из последних просмотренных фильмов', '');
+//     const b = prompt('На сколько оцените его', '');
+//     i++;
+//     if ( a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done')
+//     } else {
+//         alert('Ошибка, давайте по новой!')
+//         i--;
+//     }
+//     console.log('done1');
+// }
+
+if (personalMovieDB.count < 10){
+    alert('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель')
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман')
+}
+else alert('Произошла ошибка')
 
 
 console.log(personalMovieDB);
