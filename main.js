d61146e3ab66390c1cfac8cef59eef3a4296a37d
“Andru6a” <andrew.pmnv@gmail.com>
2023-10-16 11:07:55 +0300

    third app lesson 29 v2

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ let personalMovieDB = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов', '');
+        const a = prompt('Один из последних просмотренных фильмов', '').trim();
         const b = prompt('На сколько оцените его', '');
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMovieDB.movies[a] = b;
